6877b83bc70ce97ab88711cc9fb76797e9729c0f
codecustard <109494546+vimalveeramani@users.noreply.github.com>
2023-08-18 09:12:38 +0530

    Create syntax.go

---
+++ syntax.go
@@ -0,0 +1,13 @@
+package main
+import ("fmt")
+
+func main() {
+  fmt.println("Hello World!")
+}
+/*A Go file consists of the following parts:
+
+Package declaration
+Import packages
+Functions
+Statements and expressions
+*/
